ba7319d43f7a39f4f65b86cdbf3170ce095eab66
Niels Vergucht <vergucht.niels@gmail.com>
2018-12-03 17:22:27 +0100

    Added getter for ignoredmaterials

--- src/main/java/com/songoda/epicbuckets/file/ConfigManager.java
+++ src/main/java/com/songoda/epicbuckets/file/ConfigManager.java
@@ -95,4 +95,8 @@ public class ConfigManager {
     public String getBulkShopPurchasePath() {
         return bulkShopPurchasePath;
     }
+
+    public List<String> getIgnoredMaterials() {
+        return ignoredMaterials;
+    }
 }
