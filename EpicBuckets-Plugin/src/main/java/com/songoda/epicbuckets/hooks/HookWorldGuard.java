1a32b066e92fb08199886413e8e0ffb2b93c9ab5
Esophose <esophose@gmail.com>
2019-04-01 12:49:36 -0600

    Dependencies, worldguard 7.0.0 integration

--- EpicBuckets-Plugin/src/main/java/com/songoda/epicbuckets/hooks/HookWorldGuard.java
+++ EpicBuckets-Plugin/src/main/java/com/songoda/epicbuckets/hooks/HookWorldGuard.java
@@ -1,6 +1,10 @@
 package com.songoda.epicbuckets.hooks;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.songoda.epicbuckets.utils.hooks.ProtectionPluginHook;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -21,7 +25,11 @@ public class HookWorldGuard implements ProtectionPluginHook {
 
     @Override
     public boolean canBuild(Player player, Location location) {
-        return worldGuard.canBuild(player, location);
+        RegionManager regionManager = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(location.getWorld()));
+        if (regionManager == null) return true;
+        ApplicableRegionSet set = regionManager.getApplicableRegions(BukkitAdapter.asBlockVector(location));
+
+        return set.size() < 1;
     }
 
 }
